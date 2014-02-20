a75ec26ba1f16709d32b8064563e281b4dc940bf
Doct <Doct@Doct-PC>
2014-02-20 11:55:49 +0100

    Commit achref

--- src/github/Github.java
+++ src/github/Github.java
@@ -14,6 +14,5 @@ public class Github {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        System.out.println("");    }
 }
